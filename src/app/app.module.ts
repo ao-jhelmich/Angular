45c44f5197ca0128e5b95eaa93a500c7c9f06e1c
Jasper Helmich <jasper.helmich@gmail.com>
2018-12-19 23:23:14 +0100

    Basic crud functionality!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
+// Angular modules
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
+// Api modules
+import { HttpClientModule } from '@angular/common/http';
+import { InMemoryDataService }  from './services/in-memory-data.service'; // Memory class
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api'; // Api inside project for testing and dev
+
+// All components
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './components/heroes/heroes.component';
-import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
-import { MessagesComponent } from './components/messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { MessagesComponent } from './components/messages/messages.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		HeroesComponent,
-		HeroDetailComponent,
 		MessagesComponent,
-		DashboardComponent
+		DashboardComponent,
+		HeroDetailComponent,
 	],
 	imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule,
-    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
 	],
 	providers: [],
 	bootstrap: [AppComponent]
